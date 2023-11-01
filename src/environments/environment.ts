42461894b28ea1f1315929c7130ca752aa93227a
anilmsd <96515732+anilmsd@users.noreply.github.com>
2023-11-01 17:38:25 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,8 @@ export const environment={
     // production:true,
     //prod
     
-      apiUrl:'http://localhost:4000/form'
-      // apiUrl:'https://irecruit.spectraforce.com/api/iRecruit_Backend-0.0.1-SNAPSHOT/iRecruit'
+      //apiUrl:'http://localhost:4000/form'
+      apiUrl:'http://16.171.57.230:4000/form'
   
     
       // apiUrl:'http://localhost:9090/iRecruit'
