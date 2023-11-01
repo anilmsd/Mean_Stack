ed9834aac4e2d2321d52200e9079e0dae2cef4f5
anilmsd <96515732+anilmsd@users.noreply.github.com>
2023-11-01 17:40:03 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,5 @@ export const environment={
       //apiUrl:'http://localhost:4000/form'
       apiUrl:'http://16.171.57.230:4000/form'
   
-    
       // apiUrl:'http://localhost:9090/iRecruit'
   }
